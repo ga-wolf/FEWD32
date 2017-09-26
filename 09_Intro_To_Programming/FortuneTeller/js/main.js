848f07973fbec35ad7c8f1fc7e4c3a2c834a239b
ga-wolf <jack.jeffress@ga.co>
2017-09-26 21:05:29 +1000

    Started JavaScript

---
+++ 09_Intro_To_Programming/FortuneTeller/js/main.js
@@ -0,0 +1,23 @@
+// The Fortune Teller
+
+// Why pay a fortune teller when you can just program your fortune yourself?
+
+// Store the following into variables: number of children, partner's name, geographic location, job title. Output your fortune to the screen like so: "You will be a X in Y, and married to Z with N kids."
+
+var numChildren = 100;
+var partnerName = "Bill Murray";
+var geoLocation = "The Ghetto";
+var jobTitle = "Ping pong player";
+
+var fortune =
+  "You will be a " +
+  jobTitle +
+  " in " +
+  geoLocation +
+  ", and married to " +
+  partnerName +
+  " with " +
+  numChildren +
+  " kids.";
+
+console.log(fortune);
---
+++ 09_Intro_To_Programming/js/main.js
@@ -0,0 +1,64 @@
+console.log("Hello World");
+
+("Hello World");
+
+("Hello Somebody");
+
+("Jane's Bag");
+
+('He said "Hello"');
+
+// Properties
+// Static information about a piece of data
+console.log("Hello".length);
+
+// Methods
+// Set of code that can be executed on a piece of data
+console.log("hello".toUpperCase());
+
+// Concatenation
+// Combining two strings
+console.log("Hello " + "World");
+
+// Indexing
+// It is always zero-based! Meaning 0 is the first item
+console.log("hello"[0]);
+console.log("hello"[4]);
+
+// NUMBERS
+
+console.log(1 + 212.214); // Addition
+console.log(15 - 3); // Subtraction
+console.log(3 * 5); // Multiplication
+console.log(5 / 2); // Division
+console.log(9 % 2); // Modulus Operator (remainder)
+console.log(8 % 2);
+
+console.log(9 > 8);
+console.log(8 <= 10);
+console.log(9 === 9);
+console.log(8 !== 1);
+
+// BOOLEANS
+
+console.log(true);
+console.log(false);
+
+// VARIABLES
+// Declaration and assignment
+// It creates a new JS identifier and gives that a value
+
+var name = "James Joyce";
+var course = "FEWD";
+var courseNumber = 32;
+var isThisJavaScript = true;
+var fullCourseName = course + " " + courseNumber;
+console.log(fullCourseName);
+
+var someVar = 42;
+someVar = "something else";
+someVar = false;
+console.log(someVar);
+
+// camelCase
+// Every word after the first one is capitalised
